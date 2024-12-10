49e8dac2ea440c2b7593cfdf3c946de31491edcf
kittypitty <samutic40@gmail.com>
2024-12-10 12:53:23 +0300

    feature: create OnboardingPage

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -3,7 +3,8 @@ import {Route, Router} from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/invites" page={InviteInvitesPage} name="invites" />
+      <Route path="/onboarding" page={OnboardingPage} name="onboarding" />
+      <Route path="/invites" page={InvitesPage} name="invites" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
